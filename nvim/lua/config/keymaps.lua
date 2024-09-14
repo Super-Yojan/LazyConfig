3132e0cd7ff8018cc1ce88e6af74ce7e1d829228
Super-Yojan <gautamyojan0@gmail.com>
2024-09-13 22:11:12 -0400

    added latex live preview keybind

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,26 @@ map("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Tmux Left" })
 map("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Tmux Down" })
 map("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Tmux Up" })
 map("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Tmux Right" })
+
+-- set shorter name for keymap function
+local kmap = vim.keymap.set
+
+-- F5 processes the document once, and refreshes the view
+kmap({ "n", "v", "i" }, "<F5>", function()
+  require("knap").process_once()
+end)
+
+-- F6 closes the viewer application, and allows settings to be reset
+kmap({ "n", "v", "i" }, "<F6>", function()
+  require("knap").close_viewer()
+end)
+
+-- F7 toggles the auto-processing on and off
+kmap({ "n", "v", "i" }, "<F7>", function()
+  require("knap").toggle_autopreviewing()
+end)
+
+-- F8 invokes a SyncTeX forward search, or similar, where appropriate
+kmap({ "n", "v", "i" }, "<F8>", function()
+  require("knap").forward_jump()
+end)
