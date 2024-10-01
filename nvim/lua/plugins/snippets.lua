098c7fc91e9b680af961c94ec35e7d03c2468e1f
Yojan Gautam <ygautam2@gmu.edu>
2024-10-01 10:56:26 -0400

    Lua snippet and live preview

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -1,7 +1,22 @@
 return {
 
-  "rafamadriz/friendly-snippets",
-  setup = function()
-    require("utils").packer_lazy_load("friendly-snippets")
-  end,
+  { "rafamadriz/friendly-snippets" },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      -- will exclude all javascript snippets
+      require("luasnip.loaders.from_vscode").load({
+        exclude = { "javascript" },
+      })
+
+      -- load snippets from path/of/your/nvim/config/my-cool-snippets
+      -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-cool-snippets" } })
+    end,
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+  },
+  -- require("luasnip.loaders.from_vscode").lazy_load(),
 }
